f2d0246fb5426d7173ffe18a3a01d45b83248cd2
Guillermo Ruffino <guillermo.ruffino@pampatech.com>
2026-03-24 10:57:19 -0300

    Update to share on projects

--- CANopenNode_ESP32.c
+++ CANopenNode_ESP32.c
@@ -3,8 +3,9 @@
 #if CONFIG_USE_CANOPENNODE
 
 #include "esp_log.h"
-#include "CANopen.h"
+#include "CANopenNode_ESP32.h"
 #include "OD.h"
+#include "OD_extensions.h"
 
 #if (CONFIG_FREERTOS_HZ != 1000)
 #error "FreeRTOS tick interrupt frequency must be 1000Hz"
@@ -14,10 +15,7 @@
 
 static const char *TAG = "CO_ESP32";
 
-/* default values for CO_CANopenInit() */
-#define NMT_CONTROL (CO_NMT_STARTUP_TO_OPERATIONAL | CO_NMT_ERR_ON_ERR_REG | CO_ERR_REG_GENERIC_ERR | CO_ERR_REG_COMMUNICATION)
-
-static CO_t *CO = NULL;
+CO_t *CO = NULL;
 static void *CANptr = NULL;
 
 static StaticTask_t xCoMainTaskBuffer;
@@ -30,143 +28,164 @@ static StackType_t xCoPeriodicStack[CONFIG_CO_PERIODIC_TASK_STACK_SIZE];
 static TaskHandle_t xCoPeriodicTaskHandle = NULL;
 static void CO_periodicTask(void *pxParam);
 
-bool CO_ESP32_init()
+static SemaphoreHandle_t xPeriodicTaskSemaphore, xProcessTaskSemaphore;
+static uint8_t active_node_id = CONFIG_CO_DEFAULT_NODE_ID;
+
+#if (CO_CONFIG_STORAGE) & CO_CONFIG_STORAGE_ENABLE
+static CO_storage_t storage;
+#endif
+
+/* -------------------------------------------------------------------------- */
+
+void CO_ESP32_alloc(uint8_t node_id)
 {
-    ESP_LOGI(TAG, "Initializing");
+    ESP_LOGI(TAG, "Initializing Node %d", node_id);
+    od_extensions_init_extensions();
+
+    active_node_id = node_id;
+    uint32_t heapMemoryUsed;
+    xPeriodicTaskSemaphore = xSemaphoreCreateBinary();
+    xProcessTaskSemaphore = xSemaphoreCreateBinary();
+
+    CO = CO_new(NULL, &heapMemoryUsed);
+    if (CO == NULL)
+        ESP_LOGW(TAG, "Can't allocate memory");
+    else
+        ESP_LOGI(TAG, "Allocated %d bytes for CANopen objects", (int)heapMemoryUsed);
+}
+
+void CO_ESP32_storage_init(CO_storage_entry_t *entries, uint8_t entry_count)
+{
+#if (CO_CONFIG_STORAGE) & CO_CONFIG_STORAGE_ENABLE
+    uint32_t storageInitError = 0;
+    CO_ReturnError_t err = CO_storageNvs_init(&storage, CO->CANmodule,
+                                              OD_ENTRY_H1010_storeParameters,
+                                              OD_ENTRY_H1011_restoreDefaultParameters,
+                                              entries, entry_count, &storageInitError);
+    if (err != CO_ERROR_NO && err != CO_ERROR_DATA_CORRUPT)
+        ESP_LOGE(TAG, "Error: Storage %lu", storageInitError);
+#endif
+}
+
+void CO_ESP32_start_task(void)
+{
     xCoMainTaskHandle = xTaskCreateStaticPinnedToCore(
-        CO_mainTask,
-        "CO_main",
-        CONFIG_CO_MAIN_TASK_STACK_SIZE,
-        (void *)0,
-        CONFIG_CO_MAIN_TASK_PRIORITY,
-        &xCoMainStack[0],
-        &xCoMainTaskBuffer,
-        CONFIG_CO_TASK_CORE);
+        CO_mainTask, "CO_main",
+        CONFIG_CO_MAIN_TASK_STACK_SIZE, (void *)0,
+        CONFIG_CO_MAIN_TASK_PRIORITY, &xCoMainStack[0],
+        &xCoMainTaskBuffer, CONFIG_CO_TASK_CORE);
+}
+
+bool CO_ESP32_run(uint8_t node_id, CO_storage_entry_t *entries, uint8_t entry_count)
+{
+    CO_ESP32_alloc(node_id);
+    CO_ESP32_storage_init(entries, entry_count);
+    CO_ESP32_start_task();
     return true;
 }
+
+/* -------------------------------------------------------------------------- */
+
+static void SignalPeriodicTask(void *)
+{
+    xSemaphoreGive(xPeriodicTaskSemaphore);
+}
+
+#if (CO_CONFIG_SDO_SRV) & CO_CONFIG_FLAG_CALLBACK_PRE
+static void SignalProcessTask(void *)
+{
+    xSemaphoreGive(xProcessTaskSemaphore);
+}
+#endif
 
 static void CO_mainTask(void *pxParam)
 {
     CO_ReturnError_t err;
     uint32_t errInfo = 0;
     CO_NMT_reset_cmd_t reset = CO_RESET_NOT;
-    uint32_t heapMemoryUsed;
-    uint8_t activeNodeId = CONFIG_CO_DEFAULT_NODE_ID;
     TickType_t xLastWakeTime;
 
     ESP_LOGI(TAG, "main task running.");
 
-    /* Allocate CANopen object */
-    CO = CO_new(NULL, &heapMemoryUsed);
-    if (CO == NULL)
-    {
-        ESP_LOGW(TAG, "Can't allocate memory");
-    }
-    else
-    {
-        ESP_LOGI(TAG, "Allocated %d bytes for CANopen objects", (int)heapMemoryUsed);
-    }
-
     while (reset != CO_RESET_APP)
     {
-        /* CANopen communication reset - initialize CANopen objects *******************/
         ESP_LOGI(TAG, "CANopenNode - Reset communication");
 
         CO->CANmodule->CANnormal = false;
-
-        /* Enter CAN configuration. */
         CO_CANsetConfigurationMode(CANptr);
 
-        /* Initialize CAN */
         err = CO_CANinit(CO, CANptr, CONFIG_CO_DEFAULT_BPS);
         if (err != CO_ERROR_NO)
-        {
             ESP_LOGE(TAG, "CAN initialization failed: %d", err);
-        }
 
-        /* Initialize CANopen */
-        err = CO_CANopenInit(CO,                           /* CANopen object */
-                             NULL,                         /* alternate NMT */
-                             NULL,                         /* alternate em */
-                             OD,                           /* Object dictionary */
-                             NULL,                         /* Optional OD_statusBits */
-                             NMT_CONTROL,                  /* CO_NMT_control_t */
-                             CONFIG_CO_FIRST_HB_TIME,      /* firstHBTime_ms */
-                             CONFIG_CO_SDO_SERVER_TIMEOUT, /* SDOserverTimeoutTime_ms */
-                             CONFIG_CO_SDO_CLIENT_TIMEOUT, /* SDOclientTimeoutTime_ms */
+        err = CO_CANopenInit(CO,
+                             NULL, NULL,
+                             OD, NULL,
+                             CO_NMT_STARTUP_TO_OPERATIONAL,
+                             CONFIG_CO_FIRST_HB_TIME,
+                             CONFIG_CO_SDO_SERVER_TIMEOUT,
+                             CONFIG_CO_SDO_CLIENT_TIMEOUT,
 #if CONFIG_CO_SDO_CLIENT_BLOCK_TRANSFER
-                             true, /* SDOclientBlockTransfer */
+                             true,
 #else
-                             false, /* SDOclientBlockTransfer */
+                             false,
 #endif
-                             activeNodeId,
-                             &errInfo);
+                             active_node_id, &errInfo);
         if ((err != CO_ERROR_NO) && (err != CO_ERROR_NODE_ID_UNCONFIGURED_LSS))
         {
             if (err == CO_ERROR_OD_PARAMETERS)
-            {
                 ESP_LOGE(TAG, "Object Dictionary entry 0x%lx", errInfo);
-            }
             else
-            {
                 ESP_LOGE(TAG, "CANopen initialization failed: %d", err);
-            }
         }
+        CO_SYNC_initCallbackPre(CO->SYNC, NULL, SignalPeriodicTask);
 
-        err = CO_CANopenInitPDO(CO, CO->em, OD, activeNodeId, &errInfo);
+        err = CO_CANopenInitPDO(CO, CO->em, OD, active_node_id, &errInfo);
         if (err != CO_ERROR_NO)
         {
             if (err == CO_ERROR_OD_PARAMETERS)
-            {
                 ESP_LOGE(TAG, "Object Dictionary entry 0x%lx", errInfo);
-            }
             else
-            {
                 ESP_LOGE(TAG, "PDO initialization failed: %d", err);
-            }
         }
+        CO_RPDO_initCallbackPre(&CO->RPDO[0], NULL, SignalPeriodicTask);
 
-        /*
-         * Create Timer Task with execution every 1 millisecond
-         */
+#if (CO_CONFIG_SDO_SRV) & CO_CONFIG_FLAG_CALLBACK_PRE
+        CO_SDOserver_initCallbackPre(CO->SDOserver, NULL, SignalProcessTask);
+#endif
+
 #if (CONFIG_CO_PERIODIC_TASK_PRIORITY <= CONFIG_CO_MAIN_TASK_PRIORITY)
-/*
- * Note: taskCO_timer priority must be higher than taskCO_main priority
- */
+/* Note: taskCO_timer priority must be higher than taskCO_main priority */
 #error "Invalid CANopenNode task priority"
 #endif
         if (xCoPeriodicTaskHandle == NULL)
         {
             ESP_LOGI(TAG, "creating periodic task");
             xCoPeriodicTaskHandle = xTaskCreateStaticPinnedToCore(
-                CO_periodicTask,
-                "CO_timer",
-                CONFIG_CO_PERIODIC_TASK_STACK_SIZE,
-                (void *)0,
-                CONFIG_CO_PERIODIC_TASK_PRIORITY,
-                &xCoPeriodicStack[0],
-                &xCoPeriodicTaskBuffer,
-                CONFIG_CO_TASK_CORE);
+                CO_periodicTask, "CO_timer",
+                CONFIG_CO_PERIODIC_TASK_STACK_SIZE, (void *)0,
+                CONFIG_CO_PERIODIC_TASK_PRIORITY, &xCoPeriodicStack[0],
+                &xCoPeriodicTaskBuffer, CONFIG_CO_TASK_CORE);
             if (xCoPeriodicTaskHandle == NULL)
-            {
                 ESP_LOGE(TAG, "Failed to create periodic task");
-            }
         }
         ESP_LOGI(TAG, "periodic task created");
 
 #if CO_CONFIG_LEDS
         CO_LEDs_init(CO->LEDs);
 #endif
-
-        /* Start CAN */
         CO_CANsetNormalMode(CO->CANmodule);
         reset = CO_RESET_NOT;
         ESP_LOGI(TAG, "CANopenNode is running");
+
         xLastWakeTime = xTaskGetTickCount();
         while (reset == CO_RESET_NOT)
         {
+#if (CO_CONFIG_SDO_SRV) & CO_CONFIG_FLAG_CALLBACK_PRE
+            xSemaphoreTake(xProcessTaskSemaphore, pdMS_TO_TICKS(CONFIG_CO_MAIN_TASK_INTERVAL_MS));
+#else
             vTaskDelayUntil(&xLastWakeTime, CONFIG_CO_MAIN_TASK_INTERVAL_MS);
-            /* CANopen process */
+#endif
             reset = CO_process(CO, false, CO_MAIN_TASK_INTERVAL_US, NULL);
 #if CO_CONFIG_LEDS
             uint32_t ledState;
@@ -176,7 +195,7 @@ static void CO_mainTask(void *pxParam)
             gpio_set_level(CONFIG_CO_LED_RED_GPIO, ledState);
 #else
             gpio_set_level(CONFIG_CO_LED_RED_GPIO, (ledState == 0) ? 1 : 0);
-#endif /* CONFIG_CO_LED_RED_ACTIVE_HIGH */
+#endif
 #endif
 #if (CONFIG_CO_LED_GREEN_GPIO >= 0)
             ledState = CO_LED_GREEN(CO->LEDs, CO_LED_CANopen);
@@ -184,20 +203,16 @@ static void CO_mainTask(void *pxParam)
             gpio_set_level(CONFIG_CO_LED_GREEN_GPIO, ledState);
 #else
             gpio_set_level(CONFIG_CO_LED_GREEN_GPIO, (ledState == 0) ? 1 : 0);
-#endif /* CONFIG_CO_LED_GREEN_ACTIVE_HIGH */
+#endif
 #endif
 #endif /* CO_CONFIG_LEDS */
         }
     }
 
     CO_delete(CO);
-
-    /* Reset */
     ESP_LOGI(TAG, "resetting");
-    vTaskDelay(100); /* Put some delay to give time to dump serial log */
+    vTaskDelay(100);
     esp_restart();
-
-    /* Will not reach here */
     vTaskDelete(NULL);
 }
 
@@ -207,7 +222,7 @@ static void CO_periodicTask(void *pxParam)
 
     while (1)
     {
-        vTaskDelay(1);
+        xSemaphoreTake(xPeriodicTaskSemaphore, pdMS_TO_TICKS(1));
         if ((!CO->nodeIdUnconfigured) && (CO->CANmodule->CANnormal))
         {
             bool syncWas = false;
@@ -221,6 +236,61 @@ static void CO_periodicTask(void *pxParam)
             CO_process_TPDO(CO, syncWas, CO_PERIODIC_TASK_INTERVAL_US, NULL);
 #endif
         }
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+void CO_consumer_heartbeat_init(uint8_t node_id, uint16_t timeout_ms)
+{
+    uint32_t value = ((uint32_t)node_id << 16) | timeout_ms;
+    OD_PERSIST_COMM.x1016_consumerHeartbeatTime[OD_PERSIST_COMM.x1016_consumerHeartbeatTime_sub0++] = value;
+}
+
+CO_HBconsNode_t *CO_consumer_find(uint8_t node_id)
+{
+    for (uint8_t i = 0; i < CO->HBcons->numberOfMonitoredNodes; i++)
+    {
+        CO_HBconsNode_t *node = &CO->HBcons->monitoredNodes[i];
+        if (node->nodeId == node_id)
+            return node;
+    }
+    ESP_LOGE(TAG, "Cannot find hb consumer for node id %d", node_id);
+    ESP_ERROR_CHECK(ESP_ERR_INVALID_STATE);
+    return NULL;
+}
+
+const char *hb_state_str(CO_HBconsumer_state_t state)
+{
+    switch (state)
+    {
+    case CO_HBconsumer_UNCONFIGURED:
+        return "Unconfigured";
+    case CO_HBconsumer_ACTIVE:
+        return "Active";
+    case CO_HBconsumer_TIMEOUT:
+        return "Timeout";
+    case CO_HBconsumer_UNKNOWN:
+    default:
+        return "Unknown";
+    }
+}
+
+const char *nmt_state_str(CO_NMT_internalState_t state)
+{
+    switch (state)
+    {
+    case CO_NMT_INITIALIZING:
+        return "Initializing";
+    case CO_NMT_PRE_OPERATIONAL:
+        return "Pre_operational";
+    case CO_NMT_OPERATIONAL:
+        return "Operational";
+    case CO_NMT_STOPPED:
+        return "Stopped";
+    case CO_NMT_UNKNOWN:
+    default:
+        return "Unknown";
     }
 }
 
--- CANopenNode_ESP32.h
+++ CANopenNode_ESP32.h
@@ -1,11 +1,23 @@
 #ifndef CANOPENNODE_ESP32_H
 #define CANOPENNODE_ESP32_H
 
-#if CONFIG_USE_CANOPENNODE
-
 #include "CANopen.h"
+#include "CO_nvs.h"
 
-bool CO_ESP32_init();
+extern CO_t *CO;
 
-#endif /* CONFIG_USE_CANOPENNODE */
+/* All-in-one init (allocate + storage + start task) */
+bool CO_ESP32_run(uint8_t node_id, CO_storage_entry_t *entries, uint8_t entry_count);
+
+/* Split init steps (for projects that need to read OD values between storage and task start) */
+void CO_ESP32_alloc(uint8_t node_id);
+void CO_ESP32_storage_init(CO_storage_entry_t *entries, uint8_t entry_count);
+void CO_ESP32_start_task(void);
+
+/* Heartbeat consumer utilities */
+void CO_consumer_heartbeat_init(uint8_t node_id, uint16_t timeout_ms);
+CO_HBconsNode_t *CO_consumer_find(uint8_t node_id);
+const char *hb_state_str(CO_HBconsumer_state_t state);
+const char *nmt_state_str(CO_NMT_internalState_t state);
+
 #endif /* CANOPENNODE_ESP32_H */
